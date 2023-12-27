d5365babf7b62b035d9f9f9a99c2a3f81281953d
Nweke273 <chisom5710@gmail.com>
2023-12-27 04:14:18 -0800

    Feat: Boostrap project, create landing page

---
+++ lib/main.dart
@@ -0,0 +1,10 @@
+import 'dart:math';
+
+import 'package:flutter/material.dart';
+import 'package:lara_quiz/widgets/quiz.dart';
+
+void main() {
+  runApp(
+    const Quiz(),
+  );
+}
---
+++ lib/widgets/landing_page.dart
@@ -0,0 +1,36 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class LandingPage extends StatelessWidget {
+  const LandingPage({Key? key}) : super(key: key);
+  final String heading = "Learn Laravel the fun way";
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Image.asset(
+            'assets/images/quiz-logo.png',
+            width: 300,
+            color: const Color.fromARGB(150, 255, 255, 255),
+          ),
+          const SizedBox(height: 80),
+          Text(
+            heading,
+            style: const TextStyle(
+                color: Color.fromARGB(255, 237, 223, 252), fontSize: 24),
+          ),
+          const SizedBox(height: 30),
+          OutlinedButton.icon(
+              onPressed: () {
+                print("Lets start quiz");
+              },
+              style: OutlinedButton.styleFrom(foregroundColor: Colors.white),
+              icon: const Icon(Icons.arrow_right_alt),
+              label: const Text('Start Quiz'))
+        ],
+      ),
+    );
+  }
+}
